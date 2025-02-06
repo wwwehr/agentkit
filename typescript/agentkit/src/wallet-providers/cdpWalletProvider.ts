0a6e3f1d8eaa2833ef6d63bfc5957650ff26966a
debuggingfuture (Vincent LCY) <lauchunyin@debuggingfuture.com>
2025-02-07 05:01:42 +0800

    fix: use cdp wallet address at estimateGas (#283)

--- typescript/agentkit/src/wallet-providers/cdpWalletProvider.ts
+++ typescript/agentkit/src/wallet-providers/cdpWalletProvider.ts
@@ -274,7 +274,7 @@ export class CdpWalletProvider extends EvmWalletProvider {
     const feeData = await this.#publicClient!.estimateFeesPerGas();
 
     const gas = await this.#publicClient!.estimateGas({
-      account: this.#publicClient.account,
+      account: this.#address! as `0x${string}`,
       to,
       value,
       data,
